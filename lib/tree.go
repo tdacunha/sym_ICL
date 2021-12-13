ce9997f93d99aa37492f02ec0e74e39d36165a90
phil-mansfield <phil1@sh02-ln02.stanford.edu>
2021-12-12 16:14:43 -0800

    Wrote file-reading library in Go

--- lib/tree.go
+++ lib/tree.go
@@ -3,7 +3,9 @@ package lib
 import (
 	"encoding/binary"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 )
 
 var (
@@ -56,7 +58,10 @@ func ReadTreeHeader(f *os.File) *TreeHeader {
 	if err != nil { panic(err.Error()) }
 	
 	err = binary.Read(f, ByteOrder, &hd.FixedHeader)
+	if err != nil { panic(err.Error()) }
 	hd.Cols = make([]int32, hd.NInt + hd.NFloat + hd.NVector)
+	err = binary.Read(f, ByteOrder, hd.Cols)
+	if err != nil { panic(err.Error()) }
 
 	return hd
 }
@@ -90,4 +95,107 @@ func ReadTreeVar(f *os.File, varName string, buf interface{}) {
 	if err != nil { panic(err.Error()) }
 	err = binary.Read(f, ByteOrder, buf)
 	if err != nil { panic(err.Error()) }
+}
+
+func BranchesName(dir string) string {
+	return path.Join(dir, "branches.dat")
+}
+
+func TreeFileNames(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil { panic(err.Error()) }
+	
+	out := []string{ }
+	for i := range files {
+		name := files[i].Name()
+		if len(name) >= 7 &&
+			name[len(name)-7:] == ".df.bin" {
+			out = append(out, path.Join(dir, name))
+		}
+	}
+
+	return out
+}
+
+func TreeNTot(files []*os.File) int {
+	nTot := 0
+	for i := range files {
+		hd := ReadTreeHeader(files[i])
+		nTot += int(hd.N)
+	}
+	return nTot
+}
+
+func ReadTreeVarFullInt(files []*os.File, varName string, buf []int32) {
+	start := 0
+	for i := range files {
+		hd := ReadTreeHeader(files[i])
+		end := start + int(hd.N)
+		subBuf := buf[start: end]
+		ReadTreeVar(files[i], varName, subBuf)
+		start = end
+	}
+}
+
+func ReadTreeVarFullFloat(files []*os.File, varName string, buf []float32) {
+	start := 0
+	for i := range files {
+		hd := ReadTreeHeader(files[i])
+		end := start + int(hd.N)
+		subBuf := buf[start: end]
+		ReadTreeVar(files[i], varName, subBuf)
+		start = end
+	}
+}
+
+func ReadTreeVarFullVector(files []*os.File, varName string, buf [][3]float32) {
+	start := 0
+	for i := range files {
+		hd := ReadTreeHeader(files[i])
+		end := start + int(hd.N)
+		subBuf := buf[start: end]
+		ReadTreeVar(files[i], varName, subBuf)
+		start = end
+	}
+}
+
+type Branches struct {
+	N, CentralIdx int32
+	Starts, Ends []int32
+	IsReal, IsDisappear, IsMWSub []bool
+	PreprocessIdx []int32
+}
+
+func ReadBranches(name string) *Branches {
+	f, err := os.Open(name)
+	if err != nil { panic(err.Error()) }
+	defer f.Close()
+
+	b := &Branches{ }
+
+	err = binary.Read(f, ByteOrder, &b.N)
+	if err != nil { panic(err.Error()) }
+	err = binary.Read(f, ByteOrder, &b.CentralIdx)
+	if err != nil { panic(err.Error()) }
+
+	edges := make([]int32, b.N+1)
+	b.IsReal = make([]bool, b.N)
+	b.IsDisappear = make([]bool, b.N)
+	b.IsMWSub = make([]bool, b.N)
+	b.PreprocessIdx = make([]int32, b.N)
+	
+	err = binary.Read(f, ByteOrder, edges)
+	if err != nil { panic(err.Error()) }
+	b.Starts = edges[:len(edges) - 1]
+	b.Ends = edges[1:]
+	err = binary.Read(f, ByteOrder, b.IsReal)
+	if err != nil { panic(err.Error()) }
+	err = binary.Read(f, ByteOrder, b.IsDisappear)
+	if err != nil { panic(err.Error()) }
+	err = binary.Read(f, ByteOrder, b.IsMWSub)
+	if err != nil { panic(err.Error()) }
+	err = binary.Read(f, ByteOrder, b.PreprocessIdx)
+	if err != nil { panic(err.Error()) }
+	
+	return b
 }
