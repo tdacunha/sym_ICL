40d12f32600f18058ad152e625887da35af12f4b
Phil Mansfield <phil1@stanford.edu>
2022-02-27 15:15:16 -0800

    Adding logging functions for heap statistics

---
+++ lib/memory_logging.go
@@ -0,0 +1,17 @@
+package lib
+
+import (
+	"log"
+	"runtime"
+)
+
+func MemoryUsage() {
+	m := runtime.MemStats{ }
+	log.Println("Memory Usage:")
+
+	runtime.ReadMemStats(&m)
+	log.Printf("    In use: %5.2f GB, Total use: %5.2f GB, Idle: %5.2g GB",
+		float64(m.Alloc)/1e9, float64(m.TotalAlloc)/1e9,
+		float64(m.HeapIdle)/1e9)
+	log.Printf("   Next GC cycle: %5.2 GB", float64(m.NextGC)/1e9)
+}
