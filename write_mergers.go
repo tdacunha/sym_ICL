b5f9ed070149c33b822f0b4ec140d370e35f4351
Phil Mansfield <phil1@stanford.edu>
2022-03-01 09:56:12 -0800

    reworked write_mergers to work with new files

--- write_mergers.go
+++ write_mergers.go
@@ -5,54 +5,49 @@ import (
 	"runtime"
 	"os"
 	"encoding/binary"
-	"io/ioutil"
-	"strings"
 	"strconv"
 	"log"
-	"path"
 	
 	"github.com/phil-mansfield/lmc_ges_tracking/lib"
 )
 
-var (
-	MaxSnap = int32(235)
-	NMerger = 10
+const (
+	NPeakMin = 300
+	MaxSnap = 235
 )
-
-// Haloes is a collection of raw columns from the tree.dat files.
-type Haloes struct {
-	ID, DescID, UPID, DFID, Snap []int32
-	Mvir, Vmax []float32
-	X, V [][3]float32
-	IDTable *LookupTable
-}
 
 type LookupTable struct {
 	Order, ID []int32
 }
 
-
-// HaloTrack contains the evolution history of a single halo.
 func main() {	
-    if len(os.Args) != 2 {
-        panic(fmt.Sprintf("You must supply a file with tree names, " +
-            "output names, and z=0 MW IDs"))
-    }
+	haloIndex := -1
+    if len(os.Args) > 3 || len(os.Args) <= 1 {
+        panic("You must provide a config file.")
+    } else if len(os.Args) == 3 {
+		var err error
+		haloIndex, err = strconv.Atoi(os.Args[2])
+		if err != nil { panic(err.Error()) }
+	}
 
 	inputName := os.Args[1]
-	treeDirs, mwIDs := ParseInputFile(inputName)
+	
+	cfg := lib.ParseConfig(inputName)
 
-	log.Printf("Running on %d haloes", len(mwIDs))
+	for i := range cfg.BaseDir {
+		if haloIndex != -1 && haloIndex !=  i { continue }
+		
+		log.Printf("Running halo %d (%s)", i, cfg.BaseDir[i])
+		lib.MemoryUsage()
+		if haloIndex != -1 && i != haloIndex { continue }
 
-	for i := range treeDirs {
-		log.Println(treeDirs[i])
-		MemoryLog()
-		branchesName := lib.BranchesName(treeDirs[i])
-		fileNames := lib.TreeFileNames(treeDirs[i])
+		branchesName := lib.BranchesFileName(cfg.BaseDir[i])
+		fileNames := lib.TreeFileNames(cfg.BaseDir[i])
+		
 		files := make([]*os.File, len(fileNames))
-		for i := range files {
+		for j := range files {
 			var err error
-			files[i], err = os.Open(fileNames[i])
+			files[j], err = os.Open(fileNames[j])
 			if err != nil { panic(err.Error()) }
 		}
 		b := lib.ReadBranches(branchesName)
@@ -60,20 +55,27 @@ func main() {
 		n := lib.TreeNTot(files)
 		mvir := make([]float32, n)
 		lib.ReadTreeVarFullFloat(files, "Mvir", mvir)
+
+		log.Printf("Mpeak cutoff: %g", cfg.Mp[i]*NPeakMin)
 		
 		mpeak := Mpeak(b, mvir)
-		mIdx := FindLargestMergers(b, mpeak)
+		mIdx := FindLargestMergers(b, mpeak, float32(cfg.Mp[i]*NPeakMin))
 		runtime.GC()
+
+		log.Printf("%d haloes tracked.", len(mIdx))
+		
 		snaps := GetSnaps(files, b, n, mIdx)
 		runtime.GC()
 
-		mFile, err := os.Create(path.Join(treeDirs[i], "mergers.dat"))
+		mFile, err := os.Create(lib.MergerFileName(cfg.BaseDir[i]))
 		if err != nil { panic(err.Error()) }
 
 		WriteHeader(MaxSnap, mIdx, mFile)
 		WriteFloat(files, b, n, mIdx, snaps, "Mvir", mFile)
 		runtime.GC()
 		WriteFloat(files, b, n, mIdx, snaps, "Vmax", mFile)
+		runtime.GC()
+		WriteFloat(files, b, n, mIdx, snaps, "RVmax", mFile)
 		runtime.GC()
 		WriteInt(files, b, n, mIdx, snaps, "ID", mFile)
 		runtime.GC()
@@ -82,7 +84,7 @@ func main() {
 		WriteVector(files, b, n, mIdx, snaps, "V", mFile)
 		runtime.GC()
 
-		mFile.Close()
+		mFile.Close()		
 	}
 }
 
@@ -173,65 +175,21 @@ func Mpeak(b *lib.Branches, mvir []float32) []float32 {
 	return mpeak
 }
 
-func FindLargestMergers(b *lib.Branches, mpeak []float32) []int32 {
+func FindLargestMergers(
+	b *lib.Branches, mpeak []float32, mpeakMin float32,
+) []int32 {
+	
 	mpeak64 := make([]float64, b.N)
 	for i := range mpeak64 { mpeak64[i] = float64(mpeak[i]) }
 	order := lib.QuickSortIndex(mpeak64)
 	
 	idx := []int32{ b.CentralIdx }
 	for i := len(mpeak) - 1; i >= 0; i-- {
+		if mpeak[i] < mpeakMin { break }
 		if b.IsMWSub[order[i]] {
 			idx = append(idx, int32(order[i]))
-			if len(idx) == NMerger + 1 { break }
 		}
 	}
 
 	return idx
-}
-
-func MemoryLog() {
-	ms := &runtime.MemStats{ }
-	runtime.ReadMemStats(ms)
-	log.Printf("Allocated: %.1f In Use: %.1f Idle: %.1f\n",
-		float64(ms.Alloc) / 1e9, float64(ms.HeapInuse) / 1e9,
-		float64(ms.HeapIdle) / 1e9)
-}
-
-func ParseInputFile(fname string) ([]string, []int32) {
-	b, err := ioutil.ReadFile(fname)
-	if err != nil {
-		panic(fmt.Sprintf("Could not open input file: %s", err.Error()))
-	}
-	s := string(b)
-
-	trees, mwIDs := []string{ }, []int32{ }
-	
-	lines := strings.Split(s, "\n")
-	for i := range lines {
-		line := strings.Trim(lines[i], " ")
-		if len(line) == 0 { continue }
-
-		tok := strings.Split(lines[i], " ")
-		cols := []string{ }
-		for i := range tok {
-			if len(tok[i]) > 0 {
-				cols = append(cols, tok[i])
-			}
-		}
-		
-		if len(cols) != 2 {
-			panic(fmt.Sprintf("Line %d of %s is '%s', but you need there " +
-				"to be three columns.", i+1, fname, line))
-		}
-		
-		trees = append(trees, cols[0])
-		mwID, err := strconv.Atoi(cols[1])
-		if err != nil {
-			panic(fmt.Sprintf("Could not parse the ID on line %d of " +
-				"%s: %s", i+1, fname, cols[1]))
-		}
-		mwIDs = append(mwIDs, int32(mwID))
-	}
-	
-	return trees, mwIDs
 }
