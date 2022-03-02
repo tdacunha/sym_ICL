3d7a26d4ab9a7658d1ff4dc61b06d534b3db180e
Phil Mansfield <phil1@stanford.edu>
2022-03-02 09:38:47 -0800

    Fixed printf verb in memory logging

--- lib/memory_logging.go
+++ lib/memory_logging.go
@@ -10,9 +10,9 @@ func MemoryUsage() {
 	log.Println("Memory Usage:")
 
 	runtime.ReadMemStats(&m)
-	log.Printf("    In use: %5.2f GB, Total use: %5.2f GB, Idle: %5.2g GB",
+	log.Printf("    In use: %5.2f GB, Total use: %5.2f GB, Idle: %5.2f GB",
 		float64(m.Alloc)/1e9, float64(m.TotalAlloc)/1e9,
 		float64(m.HeapIdle)/1e9)
-	log.Printf("    Next GC cycle: %5.2 GB, GC pause time: %.2f s",
+	log.Printf("    Next GC cycle: %5.2f GB, GC pause time: %.2f s",
 		float64(m.NextGC)/1e9, float64(m.PauseTotalNs)*1e9)
 }
